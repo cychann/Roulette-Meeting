6bb6db46579579e53bb6ca556208c041030b9ea3
cychann <skcbdlhc129@naver.com>
2021-08-12 19:48:31 +0900

    회원가입 디자인

--- accountapp/forms.py
+++ accountapp/forms.py
@@ -1,15 +1,40 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.forms import ModelForm
+from django.utils.translation import gettext, gettext_lazy as _
 
 from accountapp.models import UserModel
 
 
 class RegisterForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="비밀번호",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': '비밀번호 입력(영문,숫자 조합 최소 8자)'
+        }),
+    )
+    password2 = forms.CharField(
+        label="비밀번호 확인",
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+
     class Meta:
         model = UserModel
         fields = ['username', 'password1',
                   'password2', 'nickname']
+        labels = {
+            'username': '아이디',
+            'nickname': '닉네임',
+        }
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': '아이디 입력(5자~10자)'
+                }
+            ),
+        }
 
 
 class passwordUpdateForm(UserCreationForm):
